e39e5726fef4a56b770b16e4862d594caec392dd
om-coderguy <thombareomkar123@gmail.com>
2025-03-10 01:21:09 +0530

    changes on unauthorized access

--- ecommerce-web-frontend/src/components/home/Unauthorized.vue
+++ ecommerce-web-frontend/src/components/home/Unauthorized.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
+    <div class="unauthorized">
       <h1>Unauthorized Access</h1>
       <p>You do not have permission to view this page.</p>
     </div>
   </template>
+
+  <style>
+.unauthorized {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+    justify-content: center;
+}
+
+</style>
