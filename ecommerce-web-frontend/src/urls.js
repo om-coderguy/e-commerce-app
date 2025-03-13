8974324ad521d5aad6a6e1f519605d394b8e5f32
Omkar Thombare <thombareomkar123@gmail.com>
2025-03-13 13:06:10 +0530

    new changes port running

--- ecommerce-web-frontend/src/urls.js
+++ ecommerce-web-frontend/src/urls.js
@@ -1,18 +1,18 @@
 export default () => {
-  if (window.location.href.indexOf("localhost:8081") > -1) {
+  if (window.location.href.indexOf("localhost:8084") > -1) {
     return {
-      login: "http://localhost:8080/login",
-      registerUser: "http://localhost:8080/api/users",
-      seller: "http://localhost:8080/api/sellers",
-      products: "http://localhost:8080/api/products",
-      cart: "http://localhost:8080/api/carts",
-      categories: "http://localhost:8080/api/categories",
-      orders: "http://localhost:8080/api/orders",
-      inventory:"http://localhost:8080/api/inventories",
-      subUser:"http://localhost:8080/api/subusers",
-      // topProducts: 'http://localhost:8080/api/products/top',
-      // recentProducts :'http://localhost:8080/api/products/recent',
-      // singleProduct: 'http://localhost:8080/api/products/byId',
+      login: "http://localhost:8083/login",
+      registerUser: "http://localhost:8083/api/users",
+      seller: "http://localhost:8083/api/sellers",
+      products: "http://localhost:8083/api/products",
+      cart: "http://localhost:8083/api/carts",
+      categories: "http://localhost:8083/api/categories",
+      orders: "http://localhost:8083/api/orders",
+      inventory:"http://localhost:8083/api/inventories",
+      subUser:"http://localhost:8083/api/subusers",
+      // topProducts: 'http://localhost:8083/api/products/top',
+      // recentProducts :'http://localhost:8083/api/products/recent',
+      // singleProduct: 'http://localhost:8083/api/products/byId',
     };
   }
 };
